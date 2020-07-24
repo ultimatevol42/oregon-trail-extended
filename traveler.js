8219b48f8cc6ecaafd641cd78efc8c6633110449
Christopher Haley <ultimatevol42@gmail.com>
2020-07-23 21:55:19 -0500

    done i hope

--- traveler.js
+++ traveler.js
@@ -22,3 +22,37 @@ class Traveler {
     }
     
 }
+class Doctor extends Traveler{
+    constructor (name, food, isHealthy){
+        super(name, food, isHealthy)
+    }
+    heal(traveler){
+        return traveler.isHealthy = true
+    }
+}
+class Hunter extends Traveler{
+    constructor(name, isHealthy, food = 2){
+        super(name, isHealthy)
+        this.food = food
+    }
+    hunt(){
+        return this.food += 5
+    }
+    eat(){
+        if(this.food < 2){
+            this.food = 0
+            this.isHealthy = false
+        } else {
+        this.food -= 2
+        }
+        return this.food
+    }
+    giveFood(traveler, numOfFoodUnits){
+        if(this.food - numOfFoodUnits > 0){
+            this.food -= numOfFoodUnits
+            traveler.food += numOfFoodUnits
+        } else {
+            return "Not enough food to transfer."
+        }
+    }
+}
